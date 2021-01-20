0704254bffb829c7cf2ffe552c1b9f95b1f06cec
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-20 19:30:00 +0000

    First commit for ArraysStringsExercises

---
+++ ArraysStringsExercises/ArraysAndStringsLib/Exercises.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace ArraysAndStringsLib
+{
+    public class Exercises
+    {
+        // returns a formatted address string given its components
+        public static string Address(int number, string street, string city, string postcode)
+        {
+            throw new NotImplementedException();
+        }
+        // returns a string representing a test score, written as percentage to 1 decimal place
+        public static string Scorer(int score, int outOf)
+        {
+            throw new NotImplementedException();
+        }
+
+        // returns the double represented by the string, or -999 if conversion is not possible
+        public static double ParseNum(string numString)
+        {
+            throw new NotImplementedException();
+        }
+        public static string ManipulateString(string input, int num)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
