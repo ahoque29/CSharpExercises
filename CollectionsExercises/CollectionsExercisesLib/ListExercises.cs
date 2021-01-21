c08fb9042a378db327cf9021c4015de3b480c111
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-21 14:39:26 +0000

    first commit for Collection Exercises

---
+++ CollectionsExercises/CollectionsExercisesLib/ListExercises.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionsExercisesLib
+{
+    public class ListExercises
+    {
+        // returns a list of all the integers between 1 to max inclusive
+        // that are multiples of 5
+        public static List<int> MakeFiveList(int max)
+        {
+            throw new NotImplementedException();
+        }
+
+        // return the average of all the numbers in argList 
+        public static double Average(List<double> argList)
+        {
+            return double.NaN;
+        }
+
+        // returns a list of all the strings in sourceList that start with the letter 'A' or 'a'
+        public static List<string> MakeAList(List<string> sourceList)
+        {
+            return null;
+        }
+    }
+}
