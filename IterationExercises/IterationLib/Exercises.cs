e927ab9dadedb80af60ca831c5e229a76a902fa1
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-21 13:35:09 +0000

    first commit for selection and iterations

---
+++ IterationExercises/IterationLib/Exercises.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IterationLib
+{
+    public class Exercises
+    {
+        // returns the lowest number in the array nums
+        public static int Lowest(int[] nums)
+        {
+            return 0;
+        }
+
+        // returns the sum of all numbers between 1 and n inclusive that are divisible by either 2 or 5
+        public static int SumEvenFive(int max)
+        {
+            return 0;
+        }
+
+        // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
+        // all other letters are ignored
+        public static string CountLetters(string input)
+        {
+            return String.Empty;
+        }
+    }
+}
---
+++ IterationExercises/IterationLib/Highest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IterationLib
+{
+    public class Highest
+    {
+        public static int HighestWhileLoop(int[] nums)
+        {
+            // this method should use a while loop
+            throw new NotImplementedException();
+        }
+        
+        public static int HighestForLoop(int[] nums)
+        {
+            // this method should use a for loop
+            throw new NotImplementedException();
+        }
+        
+        public static int HighestForEachLoop(int[] nums)
+        {
+            // this method should use a for-each loop
+            throw new NotImplementedException();
+        }
+
+        public static int HighestDoWhileLoop(int[] nums)
+        {
+            // this method should use a do-while loop
+            throw new NotImplementedException();
+        }
+    }
+}
