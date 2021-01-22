725711dbd28425f65e809ee0bcf63082afa373f2
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-22 01:19:32 +0000

    implemented methods for the exercises and ran the tests except for the do while loop.

--- IterationExercises/IterationLib/Exercises.cs
+++ IterationExercises/IterationLib/Exercises.cs
@@ -7,20 +7,61 @@ namespace IterationLib
         // returns the lowest number in the array nums
         public static int Lowest(int[] nums)
         {
-            return 0;
+            int lowestNumber = int.MaxValue;
+            if (nums.Length != 0)
+            {
+                lowestNumber = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (lowestNumber > nums[i])
+                    {
+                        lowestNumber = nums[i];
+                    }
+                }
+            }                       
+            return lowestNumber;
         }
 
         // returns the sum of all numbers between 1 and n inclusive that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return 0;
+            int sum = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 2 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
         }
 
         // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
         // all other letters are ignored
         public static string CountLetters(string input)
         {
-            return String.Empty;
+            int numberOfA = 0, numberOfB = 0, numberOfC = 0, numberOfD = 0;
+
+            foreach (char character in input)
+            {
+                switch (character)
+                {
+                    case 'A':
+                        numberOfA += 1;
+                        break;
+                    case 'B':
+                        numberOfB += 1;
+                        break;
+                    case 'C':
+                        numberOfC += 1;
+                        break;
+                    case 'D':
+                        numberOfD += 1;
+                        break;
+                }
+            }
+            
+            return $"A:{numberOfA} B:{numberOfB} C:{numberOfC} D:{numberOfD}";
         }
     }
 }
--- IterationExercises/IterationLib/Highest.cs
+++ IterationExercises/IterationLib/Highest.cs
@@ -7,19 +7,46 @@ namespace IterationLib
         public static int HighestWhileLoop(int[] nums)
         {
             // this method should use a while loop
-            throw new NotImplementedException();
+            int i = 0;
+            int highestNumber = nums[0];
+            while (i < nums.Length)
+            {
+                if (highestNumber < nums[i])
+                {
+                    highestNumber = nums[i];
+                }
+                i++;
+            }            
+            return highestNumber;
         }
         
         public static int HighestForLoop(int[] nums)
         {
             // this method should use a for loop
-            throw new NotImplementedException();
+            int highestNumber = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (highestNumber < nums[i])
+                {
+                    highestNumber = nums[i];
+                }
+            }
+
+            return highestNumber;
         }
         
         public static int HighestForEachLoop(int[] nums)
         {
             // this method should use a for-each loop
-            throw new NotImplementedException();
+            int highestNumber = int.MinValue;
+            foreach (int i in nums)
+            {
+                if (highestNumber < i)
+                {
+                    highestNumber = i;
+                }
+            }
+            return highestNumber;
         }
 
         public static int HighestDoWhileLoop(int[] nums)
