d9e2d48fca3222e123b5cded456a5b988e526a48
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-24 18:42:17 +0000

    I was able to put the add() method onto the gui. WIll add the rest now.

---
+++ CalculatorWPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CalculatorTests;
+
+namespace CalculatorWPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void AddButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (Int32.TryParse(FirstArgument.Text, out int x) && Int32.TryParse(SecondArgument.Text, out int y))
+			{
+				var result = CalculatorTests.Program.Add(x, y);
+				Output.Content = result;
+			}
+		}
+	}
+}
