25c83a0f198beeb0d15df1a124b1fa7777bd4281
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-27 21:31:52 +0000

    implemented the Radio class and its methods and ran the tests. Tests successful.

--- RadioOOP_MiniProject/Radio_Mini_Project/RadioApp/Radio.cs
+++ RadioOOP_MiniProject/Radio_Mini_Project/RadioApp/Radio.cs
@@ -2,7 +2,42 @@
 {
     public class Radio
     {
+        private int _channel = 1;
+        private bool _on = false;
 
+        public int Channel
+        {
+            get
+            {
+                return _channel;
+            }
+            set
+            {
+                if (_on && value > 0 && value < 5)
+                {
+                    _channel = value;
+				}
+			}
+        }
+
+        public void TurnOn()
+        {
+            _on = true;
+        }
+
+        public void TurnOff()
+        {
+            _on = false;
+        }
+
+        public string Play()
+        {
+            if (!_on)
+            {
+                return "Radio is off";
+            }
+            return $"Playing channel {Channel}";
+        }
     }
     // implement a class Radio that corresponds to the Class diagram 
     //   and specification in the Radio_Mini_Project document
