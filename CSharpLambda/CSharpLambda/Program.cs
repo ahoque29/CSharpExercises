b32eeeca85dadc0162826164629ea0fdb0f40cd1
Ashib Hoque <gaffingsiv@hotmail.com>
2021-02-02 11:31:20 +0000

    csharplambda final commit

--- CSharpLambda/CSharpLambda/Program.cs
+++ CSharpLambda/CSharpLambda/Program.cs
@@ -32,6 +32,15 @@ namespace CSharpLambda
 			};
 
 			var youngPeopleCount = people.Count(IsYoung);
+			var youngPeopleCount2 = people.Count(p => p.Age < 30);
+			var totalAge = people.Sum(p => p.Age);
+			var averageAge = people.Average(p => p.Age);
+			var oldPeopleTotalAge = people.Sum(p => p.Age >= 30 ? p.Age : 0);
+
+			var evenDelegateCount = nums.Count(delegate (int n) { return n % 2 == 0; });
+			var evenLambdaCount = nums.Count(n => n % 2 == 0);
+
+			
 		}
 
 		private static bool IsEven(int n)
@@ -53,6 +62,8 @@ namespace CSharpLambda
 		{
 			public string Name { get; set; }
 			public int Age { get; set; }
+
+			public override string ToString() => $"{Name} is {Age} year{((Age > 1) ? "s" : "")} old";
 		}
 	}
 }
